c7ba118906d24723912eed0b8968bffb4b66dcd5
maria.martinez <maria.martinez@modalgr.io>
2024-11-16 13:48:47 -0300

    loader

--- src/app/modules/login-module/pages/layout/layout.component.ts
+++ src/app/modules/login-module/pages/layout/layout.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { LoaderService } from '../../../../core/services/loader.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.css']
+  styleUrls: ['./layout.component.css'],
 })
 export class LayoutComponent implements OnInit {
+  isLoading$: Observable<boolean>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private loaderService: LoaderService) {
+    this.isLoading$ = this.loaderService.isLoading$; // Observa o estado do loader
   }
 
+  ngOnInit() {}
 }
